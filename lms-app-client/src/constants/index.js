f263afb4b394ddba3c6f1dea7834f2da8b26d15b
Devin Herath <devdhera@gmail.com>
2018-11-07 01:02:19 +0530

    Added constatnts

---
+++ lms-app-client/src/constants/index.js
@@ -0,0 +1,24 @@
+export const API_BASE_URL = 'http://localhost:5000/api';
+//export const API_BASE_URL = '/api';
+export const ACCESS_TOKEN = 'accessToken';
+
+export const BOOK_LIST_SIZE = 30;
+export const MAX_COPIES = 6;
+export const BOOK_TITLE_MAX_LENGTH = 70;
+export const BOOK_ISBN_MAX_LENGTH = 20;
+export const BOOK_GENRE_MAX_LENGTH = 50;
+export const BOOK_CONTENT_MAX_LENGTH = 140;
+export const BOOK_AUTHOR_MAX_LENGTH = 70;
+export const BOOK_IMAGEURL_MAX_LENGTH = 140;
+export const BOOK_COPY_MAX_LENGTH = 10;
+
+export const NAME_MIN_LENGTH = 4;
+export const NAME_MAX_LENGTH = 40;
+
+export const USERNAME_MIN_LENGTH = 3;
+export const USERNAME_MAX_LENGTH = 15;
+
+export const EMAIL_MAX_LENGTH = 40;
+
+export const PASSWORD_MIN_LENGTH = 6;
+export const PASSWORD_MAX_LENGTH = 20;
