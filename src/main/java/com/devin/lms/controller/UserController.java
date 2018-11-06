5717e83671cfebc5dd8018cec4887085a8bd01b7
Devin Herath <devdhera@gmail.com>
2018-11-06 19:25:56 +0530

    Added UserController

---
+++ src/main/java/com/devin/lms/controller/UserController.java
@@ -0,0 +1,85 @@
+package com.devin.lms.controller;
+
+import com.devin.lms.exception.ResourceNotFoundException;
+import com.devin.lms.model.User;
+import com.devin.lms.payload.*;
+import com.devin.lms.repository.BookRepository;
+import com.devin.lms.repository.BorrowRepository;
+import com.devin.lms.repository.UserRepository;
+import com.devin.lms.security.CurrentUser;
+import com.devin.lms.security.UserPrincipal;
+import com.devin.lms.service.BookService;
+import com.devin.lms.util.AppConstants;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    @Autowired
+    private BorrowRepository borrowRepository;
+
+    @Autowired
+    private BookService bookService;
+
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+
+    @GetMapping("/user/me")
+    @PreAuthorize("hasRole('USER')")
+    public UserSummary getCurrentUser(@CurrentUser UserPrincipal currentUser) {
+        UserSummary userSummary = new UserSummary(currentUser.getId(), currentUser.getUsername(), currentUser.getName());
+        return userSummary;
+    }
+
+    @GetMapping("/user/checkUsernameAvailability")
+    public UserIdentityAvailability checkUsernameAvailability(@RequestParam(value = "username") String username) {
+        Boolean isAvailable = !userRepository.existsByUsername(username);
+        return new UserIdentityAvailability(isAvailable);
+    }
+
+    @GetMapping("/user/checkEmailAvailability")
+    public UserIdentityAvailability checkEmailAvailability(@RequestParam(value = "email") String email) {
+        Boolean isAvailable = !userRepository.existsByEmail(email);
+        return new UserIdentityAvailability(isAvailable);
+    }
+
+    @GetMapping("/users/{username}")
+    public UserProfile getUserProfile(@PathVariable(value = "username") String username) {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new ResourceNotFoundException("User", "username", username));
+
+        long bookCount = bookRepository.countByCreatedBy(user.getId());
+        long borrowCount = borrowRepository.countByUserId(user.getId());
+
+        UserProfile userProfile = new UserProfile(user.getId(), user.getUsername(), user.getName(), user.getCreatedAt(), bookCount, borrowCount);
+
+        return userProfile;
+    }
+
+    @GetMapping("/users/{username}/polls")
+    public PagedResponse<BookResponse> getBooksCreatedBy(@PathVariable(value = "username") String username,
+                                                         @CurrentUser UserPrincipal currentUser,
+                                                         @RequestParam(value = "page", defaultValue = AppConstants.DEFAULT_PAGE_NUMBER) int page,
+                                                         @RequestParam(value = "size", defaultValue = AppConstants.DEFAULT_PAGE_SIZE) int size) {
+        return bookService.getBooksCreatedBy(username, currentUser, page, size);
+    }
+
+    @GetMapping("/users/{username}/votes")
+    public PagedResponse<BookResponse> getBooksBorrowedBy(@PathVariable(value = "username") String username,
+                                                       @CurrentUser UserPrincipal currentUser,
+                                                       @RequestParam(value = "page", defaultValue = AppConstants.DEFAULT_PAGE_NUMBER) int page,
+                                                       @RequestParam(value = "size", defaultValue = AppConstants.DEFAULT_PAGE_SIZE) int size) {
+        return bookService.getBooksBorrowedBy(username, currentUser, page, size);
+    }
+
+}
