8c1b64299837744ab8fc8110612dde2d661a8d0a
Devin Herath <devdhera@gmail.com>
2018-11-06 17:50:01 +0530

    Added CopyResponse Payload

---
+++ src/main/java/com/devin/lms/payload/CopyResponse.java
@@ -0,0 +1,31 @@
+package com.devin.lms.payload;
+
+public class CopyResponse {
+    private long id;
+    private String status;
+    private long borrowCount;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public long getBorrowCount() {
+        return borrowCount;
+    }
+
+    public void setBorrowCount(long borrowCount) {
+        this.borrowCount = borrowCount;
+    }
+}
