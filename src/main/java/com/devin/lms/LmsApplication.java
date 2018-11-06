f4efe8e9574872aaf2121294cdfec6e3d64c0c11
Devin Herath <devdhera@gmail.com>
2018-11-06 15:44:44 +0530

    Initial commit

---
+++ src/main/java/com/devin/lms/LmsApplication.java
@@ -0,0 +1,26 @@
+package com.devin.lms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.convert.Jsr310Converters;
+
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
+@SpringBootApplication
+@EntityScan(basePackageClasses = {
+        LmsApplication.class,
+        Jsr310Converters.class
+})
+public class LmsApplication {
+
+    @PostConstruct
+    void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(LmsApplication.class, args);
+    }
+}
