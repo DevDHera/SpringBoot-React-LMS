e7b931a8e863707c7503d39ace0103a96849211e
Devin Herath <devdhera@gmail.com>
2018-11-06 17:29:02 +0530

    Added BookLength Payload

---
+++ src/main/java/com/devin/lms/payload/BookLength.java
@@ -0,0 +1,30 @@
+package com.devin.lms.payload;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotNull;
+
+public class BookLength {
+    @NotNull
+    @Max(7)
+    private Integer days;
+
+    @NotNull
+    @Max(23)
+    private Integer hours;
+
+    public Integer getDays() {
+        return days;
+    }
+
+    public void setDays(Integer days) {
+        this.days = days;
+    }
+
+    public Integer getHours() {
+        return hours;
+    }
+
+    public void setHours(Integer hours) {
+        this.hours = hours;
+    }
+}
